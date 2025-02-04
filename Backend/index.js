6763f7715080d94206c16cfc27c46f0c425cbc98
Ankit Kumar Gupta <co21309@ccet.ac.in>
2025-02-04 18:58:24 +0530

    google auth

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(
   cors({
-    origin: "http://localhost:3000", 
+    origin: process.env.CORS_URI, 
     credentials: true, 
   })
 );
@@ -21,7 +21,6 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use("/", require("./routes"));
-
 
 
 const APP_PORT = process.env.APP_PORT || 8080;
